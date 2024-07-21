b25eb890d71d61dd60efa9771e8f8bcc0f3dc584
Elite Code <atifhussain.coder@gmail.com>
2024-07-21 19:25:28 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,11 +4,7 @@ let password2 = document.querySelector("#Password2");
 let passwordVerify = document.querySelector("#Password-r")
 let seePassword = document.querySelector(" #password-see");
 
-seePassword.onclick = () => {
+seePassword.onmouseover = () => {
     password1.setAttribute('type', 'text');
     password2.setAttribute('type', 'text');
-}
-seePassword.onmousemove = () => {
-    password1.setAttribute('type', 'password');
-    password2.setAttribute('type', 'password');
 }
